78e341448a384445b6fcb78918a1b37514b4a38c
Akanimo-Umoh <akanumoh4@gmail.com>
2026-05-05 12:14:34 +0100

    feat: implement route guard for routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import { HomePage } from "./pages/home"
 import { Login } from "./pages/login"
 import { Signup } from "./pages/signup"
+import { PublicRoute } from "./routes/PublicRoute"
+// import { Chat } from "./pages/chat"
+import { ProtectedRoute } from "./routes/ProtectedRoute"
+import { Chat } from "./pages/chat"
 
 export function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+
+        <Route element={<PublicRoute />}>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/chat" element={<Chat />} />
+        </Route>
       </Routes>
     </Router>
   )
